e0fb9e8021818684670a65028216c67f3cb2e466
Marvin Roldan <marvinroldan_@hotmail.com>
2020-02-12 15:12:26 -0700

    Added methods

--- Program.cs
+++ Program.cs
@@ -6,11 +6,39 @@ namespace CSharp___Exercise_4._0
     {
         static void Main(string[] args)
         {
-           Console.WriteLine("Please enter your name: ");
+            string input = GetInput();
+            input = SmileyFace( input );
+            input = FrownyFace( input );
+            WriteName( input );
+
+           // Console.WriteLine("Please enter your name: ");
            
-           string myname = Console.ReadLine(); 
+           // string myname = Console.ReadLine(); 
            
-           Console.WriteLine (":( " +myname+ " :)" );
+           // Console.WriteLine (":( " +myname+ " :)" );
+        }
+        
+        static void WriteName(string input)
+        {
+            Console.WriteLine( input );
+        }
+
+        static string GetInput()
+        {
+            Console.WriteLine("Please enter your name: ");
+            return Console.ReadLine();
+        }
+
+        static string SmileyFace(string input)
+        {
+            input = input + ":)";
+            return input;
+        }
+
+        static string FrownyFace( string input)
+        {
+            input = input + ":(";
+            return input;    
         }
     }
 }
