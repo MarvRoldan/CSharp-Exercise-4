284b961737bf90d007e96b37805b385359343542
Marvin Roldan <marvinroldan_@hotmail.com>
2020-02-12 14:40:28 -0700

    Exercise 4

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace CSharp___Exercise_4._0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           Console.WriteLine("Please enter your name: ");
+           
+           string myname = Console.ReadLine(); 
+           
+           Console.WriteLine (":(" +myname+ ":)" );
+        }
+    }
+}
